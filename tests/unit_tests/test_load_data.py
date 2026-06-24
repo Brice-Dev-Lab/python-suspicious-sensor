0b17f886945e2c0221af104c6f3bd6c2308e65a4
Brice Nelson <bnelsonemail@icloud.com>
2026-06-24 01:07:47 -0400

    tests: add tests for testing load data function

--- tests/unit_tests/test_load_data.py
+++ tests/unit_tests/test_load_data.py
@@ -1,7 +1,17 @@
 from src.suspicious_sensor.utils.descriptive_stats_functions import load_data
 import pandas as pd
+import pytest
 
 def test_load_data():
     """Test loading data."""
     result = load_data("pressure_sensor_readings.csv")
     assert isinstance(result, pd.DataFrame)
+
+@pytest.mark.parametrize("filename", [123, None, ["file.csv"], 3.12])
+def test_load_data_filename_type(filename):
+    """Test that load_data raises TypeError for non-string filename."""
+    with pytest.raises(TypeError):
+        load_data(filename)
+
+def test_load_data_file_not_found():
+    pass
